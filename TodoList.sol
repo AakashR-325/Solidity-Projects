85cb3bbd104466225b36615b58624f2289ce6f4d
aakash <aakashramesh018@gmail.com>
2023-09-20 22:53:24 +0530

    added toggle function

--- TodoList.sol
+++ TodoList.sol
@@ -9,15 +9,19 @@ contract TodoList {
 
     Todo[] private todos;
 
-    function create(string calldata _text, bool _completed) external {
-        todos.push(Todo({text: _text, completed: _completed}));
+    function create(string calldata _text) external {
+        todos.push(Todo({text: _text, completed: false}));
     }
 
-    function updateText(uint _i, string calldata _updatedText) external {
-        todos[_i].text = _updatedText;
+    function updateText(uint _index, string calldata _updatedText) external {
+        todos[_index].text = _updatedText;
     }
 
     function get(uint _index) external view returns (Todo memory) {
         return (todos[_index]);
+    }
+
+    function toggleCompleted(uint _index) external {
+        todos[_index].completed = !todos[_index].completed;
     }
 }
